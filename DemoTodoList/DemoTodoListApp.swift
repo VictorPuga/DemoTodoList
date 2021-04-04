ead811347b22860a5b0ce509d8439669565c5103
VictorPuga <39507381+VictorPuga@users.noreply.github.com>
2021-04-04 15:19:57 -0600

    Initial Commit

---
+++ DemoTodoList/DemoTodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoTodoListApp.swift
+//  DemoTodoList
+//
+//  Created by Víctor Manuel Puga Ruiz on 04/04/21.
+//
+
+import SwiftUI
+
+@main
+struct DemoTodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
